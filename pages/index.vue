85e94fcccd5fb159dca4c75313381bc8b5607f0a
lienputnina <liene.putnina20@gmail.com>
2023-06-23 11:10:00 +0300

    refactor: adjusted the html structure of index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,40 @@
 <template>
-  <div class="flex flex-col self-center">
-    <div>
-      <input
-        id="book-search"
-        class="flex flex-grow border-slate-200 border-2 rounded h-8 text-slate-700 py-4 pl-9 bg-transparent"
-        style="
-          background-image: url(../assets/images/search.png);
-          background-repeat: no-repeat;
-          background-position-y: center;
-        "
-        name="book-search"
-        type="search"
-        v-model="userInput"
-        placeholder="Search for a book"
-      />
+  <input
+    id="book-search"
+    class="flex flex-grow border-slate-200 border-2 rounded h-8 text-slate-700 py-4 pl-9 bg-transparent"
+    style="
+      background-image: url(../assets/images/search.png);
+      background-repeat: no-repeat;
+      background-position-y: center;
+    "
+    name="book-search"
+    type="search"
+    v-model="userInput"
+    placeholder="Search for a book"
+  />
+  <div class="mt-4 text-slate-500">
+    <p>{{ resultsNumber }} results</p>
+  </div>
+  <div class="mt-2 mb-7 flex flex-col w-fit" v-if="!userInput">
+    <h2 class="text-slate-950 mb-6 font-bold text-2xl">Explore books</h2>
+    <img src="../assets/images/colouredBooks.png" alt="coloured-books" />
+  </div>
+  <!--FILTERED Books -->
+  <div class="flex flex-row m-1.5 w-fit" v-for="book in filteredData">
+    <img
+      src="../assets/images/facebookScrabble.jpeg"
+      class="w-20 h-28 rounded"
+    />
+    <div class="p-3">
+      <NuxtLink
+        :to="`/books/${book.id}`"
+        class="font-bold hover:text-slate-600"
+        >{{ book.title }}</NuxtLink
+      >
+      <p class="text-slate-700">{{ book.author }}</p>
+      <p class="text-slate-600">{{ book.year }}</p>
+      <p class="font-bold">{{ book.price }}</p>
     </div>
-    <div class="mt-4 text-slate-500">
-      <p>{{ resultsNumber }} results</p>
-    </div>
-    <div class="mt-2 flex flex-col w-fit">
-      <h2 class="text-slate-950 mb-6 font-bold text-2xl">Explore books</h2>
-      <img src="../assets/images/ColouredBooks.png" alt="coloured-books" />
-    </div>
-    <!--FILTERED Books -->
-    <ul class="flex flex-row m-1.5 w-fit" v-for="book in filteredData">
-      <img
-        src="../assets/images/FacebookScrabble.jpeg"
-        class="w-20 h-28 rounded"
-      />
-      <!-- <img src="{{ book.image }}" /> -->
-      <div class="p-3">
-        <li class="font-bold">{{ book.title }}</li>
-        <li class="text-slate-700">{{ book.author }}</li>
-        <li class="text-slate-600">{{ book.year }}</li>
-        <li class="font-bold">{{ book.price }}</li>
-      </div>
-    </ul>
   </div>
 </template>
 
